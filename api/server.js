8462e5b05dbcd9a0d68998dbed8bf9ade1d31230
Phoenix <psstout@gmail.com>
2023-10-01 20:52:45 -0700

    initial skeleton

--- api/server.js
+++ api/server.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGOURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'))
+db.once('open', () => console.log('connected'))
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
+app.use(express.json());
 
-const indexRouter = require('./routes/index.js')
-app.use(indexRouter)
+const indexRouter = require("./routes/index.js");
+app.use(indexRouter);
 
-app.listen(3000, () => 'app listening on port 3000')
+app.listen(3000, () => "app listening on port 3000");
